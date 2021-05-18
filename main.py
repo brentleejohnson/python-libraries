10f64e07ebd08eb0e8d112f081c899c1e962f66b
Brent Lee Johnson <brentleejohnson73@gmail.com>
2021-05-18 16:13:16 +0200

    Added another exercise

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ for i in range(14, 140, 14):
 
 # Exercise 2
 # Ten dates from today, 2 weeks apart
-from datetime import datetime, timedelta
+""" from datetime import datetime, timedelta
 
 # Today's date
 dt = datetime.now()
@@ -39,4 +39,24 @@ dt = datetime.now()
 # Prints 10 dates 14 days apart
 for x in range(9):
     print(dt.strftime('%Y/ %m/ %d'))
-    dt = dt + timedelta(days=7)
+    dt = dt + timedelta(days=7) """
+
+# Calculating Age
+from datetime import datetime
+year_born = int(input("Enter year born: "))
+month_born = int(input("Enter your month born: "))
+day_born = int(input("Enter your day born: "))
+
+current_year = int(datetime.today().strftime("%Y"))
+current_month = int(datetime.today().strftime("%m"))
+current_day = int(datetime.today().strftime("%d"))
+
+age = current_year - year_born - 1
+
+if month_born < current_month:
+    age += 1
+elif current_month == month_born:
+    if current_day >= day_born:
+        age += 1
+
+print(age)
